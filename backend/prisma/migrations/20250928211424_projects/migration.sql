58b71bbb048e40a0cba56e89fda5b7ebfea46b1a
plyta-git <sevenieq@gmail.com>
2025-09-29 00:47:12 +0200

    feat: integrate Tailwind CSS and DaisyUI into frontend
    - Added Tailwind CSS and DaisyUI dependencies in package.json.
    - Replaced existing CSS styles in App.css with Tailwind CSS imports.
    - Updated App.tsx to use Tailwind CSS classes for styling.
    - Removed unused assets (React logo SVG) and CSS files.
    - Configured Vite to use Tailwind CSS plugin.
    - Updated main.tsx to import the new app.css file.
    
    refactor: modify User table structure in backend
    
    - Dropped the existing nickname column from the User table.
    - Re-added the nickname column to the User table with TEXT type.
    - Dropped the User table and created a new users table with updated structure.
    - Created a new projects table with a foreign key relationship to the users table.

---
+++ backend/prisma/migrations/20250928211424_projects/migration.sql
@@ -0,0 +1,38 @@
+/*
+  Warnings:
+
+  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "public"."User";
+
+-- CreateTable
+CREATE TABLE "public"."users" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "email" TEXT NOT NULL,
+    "hash" TEXT NOT NULL,
+    "nickname" TEXT,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."projects" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "title" TEXT NOT NULL,
+    "videoUrl" TEXT NOT NULL,
+    "ownerId" INTEGER NOT NULL,
+
+    CONSTRAINT "projects_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "public"."users"("email");
+
+-- AddForeignKey
+ALTER TABLE "public"."projects" ADD CONSTRAINT "projects_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "public"."users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
